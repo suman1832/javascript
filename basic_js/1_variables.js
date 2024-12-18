82ea3e2d9f6ff150f793fbef400c9ccce815611d
suman1832 <sumankumaryadav1832@gmail.com>
2024-12-18 12:52:18 +0000

    container config done

---
+++ basic_js/1_variables.js
@@ -0,0 +1,15 @@
+const stuName = "suman"
+var stuRoll = 1234
+let stuCity = "delhi"
+stuadd = "giridih";
+
+stuCity = "suman kumar"
+stuRoll = 23546
+stuadd = "ranchi"
+
+console.log(stuCity)
+
+console.table([stuName,stuCity,stuRoll,stuadd])
+
+//we will not use var instead we use let in the code
+//const -> it's value will be constant, value won't be changed
