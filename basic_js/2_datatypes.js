a3ed9093e8645505af0b8f6c1569d15b616940e0
suman1832 <sumankumaryadav1832@gmail.com>
2024-12-20 16:29:41 +0000

    datatypes

---
+++ basic_js/2_datatypes.js
@@ -0,0 +1,26 @@
+//alert("3+4") //we are using nodejs,not browser so it won't run here.
+
+
+let name = "suman"
+let age = 20
+let isFollowed = false
+
+console.log(name)
+
+/*types of datatype
+//  number => 2 
+//  string => ""
+//  boolean => true/false
+//  bigint
+//  null => standalone value
+//  undefined => 
+//  symbol => unoqueness
+*/
+
+
+//object
+
+console.log(typeof name)
+console.log(typeof null) //object
+console.log(typeof undefined)  //undefined
+
