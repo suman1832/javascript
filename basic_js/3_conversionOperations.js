133389a698a5b69951ed9b51eb25821f6ec02266
suman1832 <sumankumaryadav1832@gmail.com>
2024-12-24 15:56:34 +0000

    conversion of datatypes

---
+++ basic_js/3_conversionOperations.js
@@ -0,0 +1,34 @@
+let score = "suman"
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber)
+//console.log(valueInNumber);
+
+
+//notes---------
+//when we convert these
+//"33" => 33
+//"33abc" => NaN(not a number)
+//true => 1 ;  false => 0
+
+let isLoggedIn = 0
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+
+//notes------------
+// 1 => true ; 0 => false
+// "" => false
+// "suman" => true
+
+let someNumber = 33
+
+let stringNumber =String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
