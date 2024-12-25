2470d3206c3fb5a0ba0ffac208ef9d25745d3c51
suman1832 <sumankumaryadav1832@gmail.com>
2024-12-25 12:20:20 +0000

    operations in js

--- basic_js/3_conversionOperations.js
+++ basic_js/3_conversionOperations.js
@@ -32,3 +32,45 @@ console.log(stringNumber);
 console.log(typeof stringNumber);
 
 
+//**********************************************Operations*********************************************************
+
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2); // to the power
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "hello"
+let str2 = "suman"
+
+let str3 = str1+str2
+// console.log(str3)
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+
+// console.log((3+4)*5%3)
+
+console.log(+true);
+console.log(+"");
+
+let num1,num2,num3
+
+num1 = num2 = num3 = 2+2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+
+
+
+
+
